c140b0b2ff0386c455bc195fb8ca29d8c5ba13b0
Yassine Bouziane <bouziane.yassine2005@gmail.com>
2024-04-30 09:46:46 +0200

    Aggiornamento db con tabella tokens

--- savewithus.sql
+++ savewithus.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 29, 2024 alle 22:48
--- Versione del server: 10.4.27-MariaDB
--- Versione PHP: 8.1.12
+-- Creato il: Apr 30, 2024 alle 09:45
+-- Versione del server: 10.1.36-MariaDB
+-- Versione PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +32,7 @@ CREATE TABLE `amministratori` (
   `id_admin` int(11) NOT NULL,
   `username` varchar(10) NOT NULL,
   `password` char(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,7 +47,7 @@ CREATE TABLE `blog` (
   `created` date NOT NULL,
   `stato` int(11) NOT NULL,
   `autore` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,26 @@ CREATE TABLE `campagne` (
   `stato` int(11) NOT NULL,
   `autore` int(11) NOT NULL,
   `luogo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cod_tokens`
+--
+
+CREATE TABLE `cod_tokens` (
+  `token` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `cod_tokens`
+--
+
+INSERT INTO `cod_tokens` (`token`, `id_user`) VALUES
+('$2y$10$WO6Em/7BcNdyVhI7ZDshDudFBZRhhjn0mlDA3AUu8SPxZZjVoMOJ.', 3),
+('$2y$10$F3Fd7cg/9K6oOfqxvtfAJuCrA.ksjwF/cmwgUAJ9SwGgrJPDgPQ6a', 4);
 
 -- --------------------------------------------------------
 
@@ -77,7 +97,7 @@ CREATE TABLE `eventi` (
   `data` date NOT NULL,
   `luogo` varchar(20) NOT NULL,
   `descrizione` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -89,7 +109,7 @@ CREATE TABLE `partecipanti_camapgne` (
   `id_user` int(11) NOT NULL,
   `id_campagna` int(11) NOT NULL,
   `commento` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -102,7 +122,7 @@ CREATE TABLE `partecipanti_eventi` (
   `id_user` int(11) NOT NULL,
   `presente_online` char(8) NOT NULL,
   `recensione` varchar(300) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -119,7 +139,7 @@ CREATE TABLE `utenti` (
   `username` varchar(10) NOT NULL,
   `password` char(60) NOT NULL,
   `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `utenti`
@@ -127,7 +147,9 @@ CREATE TABLE `utenti` (
 
 INSERT INTO `utenti` (`id_user`, `nome`, `cognome`, `email`, `num_tel`, `username`, `password`, `created`) VALUES
 (1, 'Mario', 'Rossi', '123456789', 'mariorossi@gm', 'mario79', '$2y$10$xuhFGheXxOe7mwfklOPDEudnyWWbyTnQjMovn8OLINWTDY8xHIh7y', '0000-00-00 00:00:00'),
-(2, 'Mario', 'Rossi', 'mariorossi@gmail.com', '123456789', 'mario79', '$2y$10$13HpRrhAqh7K/gSUbOul6urTNclHJrYIFcv5vJboqQfn74f.Qpgr.', '2024-04-28 19:35:05');
+(2, 'Mario', 'Rossi', 'mariorossi@gmail.com', '123456789', 'mario79', '$2y$10$13HpRrhAqh7K/gSUbOul6urTNclHJrYIFcv5vJboqQfn74f.Qpgr.', '2024-04-28 19:35:05'),
+(3, 'Anna', 'Verdi', 'annaverdi@gmail.com', '1234567890', 'anna70', '$2y$10$SweKG.kILPtNBVST5LAoXO8P9iQ19GRJOw1/E3MaQ1LNjsIYrF8qW', '2024-04-30 09:13:57'),
+(4, 'Federico', 'Rossi', 'federossi80@gmail.com', '1234567889', 'red80', '$2y$10$hSlouCFi4U7NWfJ2dO.GjuNo2c3qzOlJJFXiiG3BM58n7zS5UwXcm', '2024-04-30 09:27:41');
 
 --
 -- Indici per le tabelle scaricate
@@ -152,6 +174,13 @@ ALTER TABLE `blog`
 ALTER TABLE `campagne`
   ADD PRIMARY KEY (`id_campagna`),
   ADD KEY `vincolo_campagna_autore` (`autore`);
+
+--
+-- Indici per le tabelle `cod_tokens`
+--
+ALTER TABLE `cod_tokens`
+  ADD PRIMARY KEY (`token`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indici per le tabelle `eventi`
@@ -211,7 +240,7 @@ ALTER TABLE `eventi`
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Limiti per le tabelle scaricate
@@ -228,6 +257,12 @@ ALTER TABLE `blog`
 --
 ALTER TABLE `campagne`
   ADD CONSTRAINT `vincolo_campagna_autore` FOREIGN KEY (`autore`) REFERENCES `utenti` (`id_user`);
+
+--
+-- Limiti per la tabella `cod_tokens`
+--
+ALTER TABLE `cod_tokens`
+  ADD CONSTRAINT `cod_tokens_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `utenti` (`id_user`);
 
 --
 -- Limiti per la tabella `partecipanti_camapgne`
