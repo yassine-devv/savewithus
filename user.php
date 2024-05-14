812c2181966366f7710096e79a88cfdc492bf66b
Yassine Bouziane <bouziane.yassine2005@gmail.com>
2024-05-14 09:50:37 +0200

    Implementazione sezione commenti, e inizio user page

--- user.php
+++ user.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 session_start();
 
-if(!isset($_SESSION['iduser'])){
+if (!isset($_SESSION['iduser'])) {
     header("Location: login.php");
 }
 
@@ -9,15 +9,29 @@ if(!isset($_SESSION['iduser'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style/user.css">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
+
 <body>
-    <?php 
-    echo "Ciao ".$_SESSION['username'];
-    
-    ?>
+
+    <!-- <?php
+    echo "Ciao " . $_SESSION['username'];
+
+    ?> -->
+    <div class="right-banner">
+        <h1>Profilo</h1>
+        <a href="user.php">Dati profilo</a>
+        <a href="page=blog">Blog</a>
+        <a href="page=eventi">Eventi</a>
+        <a href="page=campagne">Campagne</a>
+    </div>
+
 </body>
+
 </html>
